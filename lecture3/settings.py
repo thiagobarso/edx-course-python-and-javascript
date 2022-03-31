c2978facd4d7ba634a196f3124bcbddc97d08e5f
Thiago Barbosa Soares <thiagosoares.barbosa@eb.mil.br>
2022-03-31 16:36:48 -0300

    lecture 4

--- lecture3/settings.py
+++ lecture3/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'tasks',
+    'flights',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
